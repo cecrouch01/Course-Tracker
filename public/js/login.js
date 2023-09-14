8296ed9a64902f5627b3863042cf40ba238cf2d0
Scott Keller <smkdbv911@gmail.com>
2023-09-14 16:50:30 -0600

    fixing post requests to api/users/login and api/users

--- public/js/login.js
+++ public/js/login.js
@@ -1,44 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const signupFields = document.querySelectorAll('.signup-field');
+document.addEventListener("DOMContentLoaded", function () {
+  const signupFields = document.querySelectorAll(".signup-field");
 
-    // JavaScript to handle the toggle action
-    document.getElementById('toggle-form').addEventListener('click', function(event) {
-        event.preventDefault();
-        const formTitle = document.getElementById('form-title');
-        
-        if (formTitle.textContent === 'Login') {
-            formTitle.textContent = 'Sign Up';
-            document.getElementById('toggle-form').textContent = 'Switch to Login';
-            signupFields.forEach(field => field.style.display = 'block');
-            
-            // Update hidden field value to sign-up
-            document.getElementById('form-action').value = 'signup';
-        } else {
-            formTitle.textContent = 'Login';
-            document.getElementById('toggle-form').textContent = 'Switch to Sign Up';
-            signupFields.forEach(field => field.style.display = 'none');
-            
-            // Update hidden field value to login
-            document.getElementById('form-action').value = 'login';
-        }
+  // JavaScript to handle the toggle action
+
+  document
+    .getElementById("toggle-form")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      const formTitle = document.getElementById("form-title");
+      const formElement = document.getElementById("login-form"); // Get the form element
+
+      if (formTitle.textContent === "Login") {
+        formTitle.textContent = "Sign Up";
+        document.getElementById("toggle-form").textContent = "Switch to Login";
+        signupFields.forEach((field) => (field.style.display = "block"));
+
+        formElement.action = "api/users"; // Set action for sign up
+
+        // Update hidden field value to sign-up
+        document.getElementById("form-action").value = "signup";
+      } else {
+        formTitle.textContent = "Login";
+        document.getElementById("toggle-form").textContent =
+          "Switch to Sign Up";
+        signupFields.forEach((field) => (field.style.display = "none"));
+
+        formElement.action = "api/users/login"; // Set action for login
+
+        // Update hidden field value to login
+        document.getElementById("form-action").value = "login";
+      }
     });
 
-    // Handle form submission
-    document.getElementById('login-form').addEventListener('submit', function(event) {
-        const action = document.getElementById('form-action').value;
+  // Handle form submission
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", function (event) {
+      const action = document.getElementById("form-action").value;
 
-        if (action === 'signup') {
-            const password = document.getElementById('password').value;
-            const confirmPassword = document.getElementById('confirm-password').value;
+      if (action === "signup") {
+        const password = document.getElementById("password").value;
+        const confirmPassword =
+          document.getElementById("confirm-password").value;
 
-            if (password !== confirmPassword) {
-                alert('Passwords do not match!');
-                event.preventDefault(); // prevent form submission
-            }
+        if (password !== confirmPassword) {
+          alert("Passwords do not match!");
+          event.preventDefault(); // prevent form submission
         }
+      }
     });
 
-    console.log("Initial state of signup fields:", signupFields[0].style.display);
+  console.log("Initial state of signup fields:", signupFields[0].style.display);
 });
 
 console.log("Script loaded");
