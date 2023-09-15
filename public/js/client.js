76b7929e2c3c10544e9987f6e23e00d7594ae8f5
Caleb Crouch <cecrouch01@gmail.com>
2023-09-14 21:41:36 -0400

    addition of web notice for authenticated users

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,23 @@
-
 async function registerServiceWorker() {
     const register = await navigator.serviceWorker.register('./worker.js', {
         scope: '/'
     });
-    const subscription = await register.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: (publicVapidKey),
-    });
-
-    await fetch("/subscribe", {
-        method: "POST",
-        body: JSON.stringify(subscription),
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
+    
+    let subscription = await register.pushManager.getSubscription();
+  
+    if(subscription === null) {
+            subscription = await register.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: publicVapidKey,
+        });
+        await fetch("/subscribe", {
+            method: "POST",
+            body: JSON.stringify(subscription),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+    }
 }
 
 
